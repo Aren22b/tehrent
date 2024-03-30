df837ecd95f3064dabbaad704caadf3a41974738
Aren <aren.b22@gmail.com>
2024-03-30 19:45:39 +0700

    Добавил функционал удаления и работы со статусами заказа

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -10,9 +10,18 @@
     <h1>Просмотр заказа №{{ $order->id }}</h1>
     <p><strong>Характер работы:</strong> {{ $order->type_of_work }}</p>
     <p><strong>Адрес:</strong> {{ $order->address }}</p>
-    <p><strong>Расстояние:</strong> {{ $order->distance }}</p>
-    <!-- Остальные детали заказа... -->
+    <p><strong>Расстояние:</strong> {{ $order->distance }} км</p>
+    <p><strong>Цена за час:</strong> {{ $order->price_per_hour }} руб.</p>
+    <p><strong>Продолжительность:</strong> {{ $order->duration }} ч.</p>
+    <p><strong>Форма оплаты:</strong> {{ $order->payment_form }}</p>
+    <p><strong>Дополнительная информация:</strong> {{ $order->additional_info }}</p>
     <a href="{{ route('orders.edit', $order) }}">Редактировать</a>
+
+    <!-- Форма для удаления заказа -->
+    <form action="{{ route('orders.destroy', $order) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit">Удалить заказ</button>
+    </form>
 </body>
 </html>
-
